3005012d420ff3628df78ca82573b14255f775b2
Jason <jadomican@gmail.com>
2017-10-27 10:46:15 +0100

    Update instance check file

--- week6/instance.check.sh
+++ week6/instance.check.sh
@@ -2,8 +2,9 @@
 
 status=$(aws ec2 describe-instance-status --instance-id $1 --query 'InstanceStatuses[*].InstanceState.Name' --output text)
 
-if [ $status = "running" ]; then
-	echo "Instance is running"
-else
+if [ -z "$status" ]; then
 	echo "Instance is not running or doesn't exist"
+elif [ $status = "running" ]; then
+        echo "Instance is running"
 fi
+

f7f9cfe9cf6ecc915df96e75977dc0b87598109f
Jason <jadomican@gmail.com>
2017-11-24 10:40:59 +0000

    Update file

--- week9/cpu_json.sh
+++ week9/cpu_json.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-mpstat -P ALL 10 1 -o JSON | jq
+echo "Timestamp"
+mpstat -o JSON | jq '.sysstat.hosts[].statistics[].timestamp'
+
+sleep 1
+
+echo "Idle Time"
+mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'
+
+sleep 1
+
+echo "10 second interval, collects 1 sample, calculate busy time"
+
+mpstat 10 1 -o JSON | jq '100 - .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'
 

23b7a89e68f3466f50c4cd83818ab4900480f9cc
Jason Domican <jadomican@gmail.com>
2017-11-26 17:41:48 +0000

    Update script to run loadtest for given period of time

--- EPA-CA2/runtest.sh
+++ EPA-CA2/runtest.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+#must have loadtest (executable) in same directory as this script
 
-echo Hello Jason
+minUsers=1
+maxUsers=50
+testduration=5		#Length of time to run loadtest for
+
+if [ $1 -lt $minUsers ] || [ $1 -gt $maxUsers ]; then	#Ensure valid number of users
+echo  "Invalid number, please enter a number between $minUsers and $maxUsers"
+else
+echo "Testing $1 users for $testduration seconds..."
+
+./loadtest $1 &		#Run loadtest in background
+
+mpstat $testduration 1 -o JSON
+
+pkill loadtest		#Kill loadtest process after running for testduration
+
+
+fi

25f241028aefba788f9129876ab821ac5b281e64
Jason Domican <jadomican@gmail.com>
2017-11-29 15:05:56 +0000

    Update script to count number of completions C0

--- EPA-CA2/runtest.sh
+++ EPA-CA2/runtest.sh
@@ -3,23 +3,24 @@
 #Must have loadtest (executable) in same directory as this script
 #Script takes one parameter - the total number of users to test
 
-minUsers=1
-maxUsers=50
-testduration=5		#Length of time (seconds) to run loadtest for
+min_users=1
+max_users=50
+test_duration=5		#Length of time (seconds) to run loadtest for
 
-if [ $1 -lt $minUsers ] || [ $1 -gt $maxUsers ]; then	#Ensure valid number of users
-echo  "Invalid number, please enter a number between $minUsers and $maxUsers"
+if [ $1 -lt $min_users ] || [ $1 -gt $max_users ]; then	#Ensure valid number of users
+echo  "Invalid number, please enter a number between $min_users and $max_users"
 else
-echo "Testing $1 users for $testduration seconds..."
+echo "Testing $1 users for $test_duration seconds..."
 
-echo -e "N \t idle" > results.dat	#-e flag allows backslash characters to be used
+echo -e "C0 \t N \t idle" > results.dat	#-e flag allows backslash characters to be used
 
-for ((i=$minUsers; i<=$1; i++))		#Number of users increases each time
+for ((i=$min_users; i<=$1; i++))		#Number of users increases each time
 do
 ./loadtest $i &		#Run loadtest in background
-output=$(mpstat $testduration 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle') #Collect 1 sample for test duration (JSON format)
+output=$(mpstat $test_duration 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle') #Collect 1 sample for test duration (JSON format)
+num_completions=$(cat synthetic.dat | wc -l)	#Number of transactions completed (C0) 
 
-echo -e "$i \t $output" >> results.dat  #Append results to results.dat
+echo -e "$num_completions \t $i \t $output" >> results.dat  #Append results to results.dat
 
 pkill loadtest		#Kill loadtest process after running for test duration
 sleep 1			#Allow time to cooldown

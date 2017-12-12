2965dfc2cec33e711ff3b773d2674646a8dd76ea
Jason Domican <jadomican@gmail.com>
2017-12-12 01:26:46 +0000

    Update max users value

--- EPA-CA2/runtest.sh
+++ EPA-CA2/runtest.sh
@@ -4,7 +4,7 @@
 #Script takes one parameter - the total number of users to test
 
 min_users=1
-max_users=50
+max_users=100
 test_duration=5		#Length of time (seconds) to run loadtest for
 
 if [ ! -f loadtest ]; then	#Check loadtest file exists before running
@@ -19,13 +19,13 @@ echo -e "C0 \t N \t idle" > results.dat	#-e flag allows backslash characters to 
 for ((i=$min_users; i<=$1; i++))		#Number of users increases each time
 do
 ./loadtest $i &		#Run loadtest in background
-output=$(mpstat $test_duration 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle') #Collect 1 sample for test duration (JSON format)
-num_completions=$(cat synthetic.dat | wc -l)	#Number of transactions completed (C0) 
 
-echo -e "$num_completions \t $i \t $output" >> results.dat  #Append results to results.dat
-
+idle_time=$(mpstat $test_duration 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle') #Collect 1 sample for test duration (JSON format)
 pkill loadtest		#Kill loadtest process after running for test duration
-sleep 1			#Allow time to cooldown
-echo "DONE $i"
+
+num_completions=$(cat synthetic.dat | grep -c "Transaction Complete")	#Get number of transactions completed (C0) 
+
+echo -e "$num_completions \t $i \t $idle_time" >> results.dat  #Append results to results.dat
+
 done
 fi
